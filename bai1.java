8e719e9a96ef4e24712efb7a8f3868fddf684781
bmt050903 <bmthu050903@gmail.com>
2026-05-15 13:11:18 +0700

    bai1

---
+++ bai1.java
@@ -0,0 +1,25 @@
+package session02;
+
+import java.util.Scanner;
+
+public class bai1 {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Nhập số nguyên dương N: ");
+      int n = Integer.parseInt(sc.nextLine());
+
+      int sum = 0;
+
+
+      if (n > 0){
+         for (int i = 1; i <= n; i++) {
+            sum += i;
+         }
+         System.out.println("Tổng từ 1 đến " + n+ " là: " + sum);
+      } else {
+         System.out.println("Số nhập vào không hợp lệ");
+      }
+
+   }
+}
